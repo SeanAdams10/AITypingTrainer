294eecabb4fae4f8ef497df648c0a0cad7219657
SeanAdams10 <sean.b.adams@gmail.com>
2025-08-23 08:38:41 -0400

    Update reset_from_git.ps1
    updated the powershell git file

--- reset_from_git.ps1
+++ reset_from_git.ps1
@@ -1,8 +1,8 @@
 # 1. Ensure you're on the right branch
-git checkout my-feature-branch
+git checkout repairing_ngram_analysis
 
 # 2. Fetch the latest info from GitHub (without merging)
 git fetch origin
 
 # 3. Reset your local branch to match the remote branch exactly
-git reset --hard origin/my-feature-branch
+git reset --hard origin/repairing_ngram_analysis
